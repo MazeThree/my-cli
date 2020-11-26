3c4506362837fd7ec6cae8571f97284b70888736
unknown <wupeng@aspire.aspire-tech.com>
2020-11-26 15:03:02 +0800

    first commit

---
+++ templateMap.js
@@ -0,0 +1,7 @@
+const templateMap = new Map()
+console.log('111')
+templateMap.set('vue-template-default',"https://github.com:MazeThree/vue-template-default#main")
+templateMap.set('mock-server',"https://github.com:channing-cli/mock-server#master")
+
+
+module.exports = templateMap
